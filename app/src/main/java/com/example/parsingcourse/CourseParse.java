350c7549e2502a942c2b543bb9b0f1a99ab9ed86
Eganin <egorzaharin12@gmail.com>
2020-09-11 01:19:03 +0500

    add jsoup parsing

--- app/src/main/java/com/example/parsingcourse/CourseParse.java
+++ app/src/main/java/com/example/parsingcourse/CourseParse.java
@@ -3,6 +3,10 @@ package com.example.parsingcourse;
 import android.os.AsyncTask;
 import android.util.Pair;
 
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -11,63 +15,65 @@ import java.net.URL;
 
 import javax.net.ssl.HttpsURLConnection;
 
-public class CourseParse extends AsyncTask<Void , Void , String> {
+public class CourseParse extends AsyncTask<Void, Void, Document> {
 
-    private static  String BASE_URL="https://ru.investing.com/currencies/%s-%s";
+    private static String BASE_URL = "https://ru.investing.com/currencies/%s-%s";
 
-    private Pair<String,String> pairSpinnerTexts;
-    private Pair<String,String> pairsTextUser;
-    private String textUserFirst ;
-    private String textUserSecond ;
+    private Pair<String, String> pairSpinnerTexts;
+    private Pair<String, String> pairsTextUser;
+    private String textUserFirst;
+    private String textUserSecond;
 
-    public CourseParse(Pair<String,String> pairSpinnerTexts ,Pair<String,String> pairsTextUser){
-        this.pairSpinnerTexts=pairSpinnerTexts;
-        this.pairsTextUser=pairsTextUser;
+    public CourseParse(Pair<String, String> pairSpinnerTexts, Pair<String, String> pairsTextUser) {
+        this.pairSpinnerTexts = pairSpinnerTexts;
+        this.pairsTextUser = pairsTextUser;
         unpackingPairs();
     }
 
     @Override
-    protected String doInBackground(Void... voids) {
-        String content = getContent(BASE_URL);
-
-        return content;
+    protected Document doInBackground(Void... voids) {
+        //String content = getContent(BASE_URL);
+        try {
+            Document page = getPage();
+            return page;
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.out.println("------------------------------------------");
+        }
+        System.out.println("------------------------------------------");
+        return null;
     }
 
     @Override
-    protected void onPostExecute(String content){
-        System.out.println(content);
+    protected void onPostExecute(Document content) {
+        String coefficientSymbol = content.select("span[id=last_last]").first().text();
+        Double coefficient = Double.parseDouble(coefficientSymbol);
+        System.out.println(coefficient);
     }
 
-    private void unpackingPairs(){
+    private void unpackingPairs() {
         textUserFirst = pairSpinnerTexts.first;
         textUserSecond = pairSpinnerTexts.second;
 
         String spinnerTextFirst = pairSpinnerTexts.first;
         String spinnerTextSecond = pairSpinnerTexts.second;
-        BASE_URL = String.format(BASE_URL,spinnerTextFirst,spinnerTextSecond);
+        BASE_URL = String.format(BASE_URL, spinnerTextFirst, spinnerTextSecond);
     }
 
-    private String getContent(String path){
-        try {
-            URL url = new URL(path);
-            HttpsURLConnection httpsURLConnection = openConnection(url);
-            BufferedReader bufferedReader = new BufferedReader(
-                    new InputStreamReader(httpsURLConnection.getInputStream()));
+    private String getContent(String path) throws IOException {
+        URL url = new URL(path);
+        HttpsURLConnection httpsURLConnection = openConnection(url);
+        BufferedReader bufferedReader = new BufferedReader(
+                new InputStreamReader(httpsURLConnection.getInputStream()));
 
-            String content = "";
-            String line ="";
+        String content = "";
+        String line = "";
 
-            while((line = bufferedReader.readLine())!=null){
-                content += line;
-            }
-            return content;
-
-        } catch (MalformedURLException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
+        while ((line = bufferedReader.readLine()) != null) {
+            content += line;
         }
-        return "";
+        return content;
+
     }
 
     private HttpsURLConnection openConnection(URL url) throws IOException {
@@ -77,6 +83,11 @@ public class CourseParse extends AsyncTask<Void , Void , String> {
         httpsURLConnection.connect();
 
         return httpsURLConnection;
+    }
+
+    private Document getPage() throws IOException {
+        Document page = Jsoup.parse(new URL(BASE_URL), 30000);
+        return page;
     }
 
 }
